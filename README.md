913ca239edb637df00014cbf23cd475f8008d1f8
Albin Larsson <mail@albinlarsson.com>
2013-11-28 02:26:37 +0100

    Added js 0.6, first draft of documentation and updated licence with my real name

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ textStretch.js
 ==============
 
 A jQuery extension for stretching a text to the width of the element.
+There's at least three other scripts for the same cause. But I didn't like them, because they weren't roubust enough, required wrappers, or didn't work intuitive, or with dynamic texts. So I wrote my own, and shelfed it until now.
+
+# How it works
+Include jQuery, if you haven't. Then the script itself.
+
+```html
+<script src="jquery.textStretch.js"></script>
+```
+Apply textStretch to a set of elements
+
+```javascript
+$(".stretch").textStretch();
+```
+Options can be added, using the json-format
+The options available are width, max- and minFontSize.
+If width is not specified it defaults to the element width (what you usually want).
+
+```javascript
+$(".stretch").textStretch({width: 300});
+```
+
+## To do
+1. Proper documentation
+2. Find out min-jQuery-version
+3. Test with web fonts again
+4. Test responsiveness 
+5. Put up forkable codepen-link
+
+## Note
+1. Doesn't work for inline or inline-block elements. It really wouldn't make sense if it did.
+2. Will break if font has been declared with "!important". Just don't do it.
+3. If using web fonts, call from window.load, not document.ready. Else it will be calculated for the fallback font.
---
+++ jquery.textStretch.js
@@ -0,0 +1,69 @@
+﻿/**
+ * textStretch.js v0.6 (2013.11.28)
+ *
+ * Copyright (c) 2012, 2013 Albin Larsson (mail@albinlarsson.com)
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ *
+ **/
+
+(function ($) {
+	"use strict";
+	//  add class for calculating width. let me know if you see this and know a better way
+	$("<style>").text(".textStretch-calc{ *display: inline; display: inline-block; white-space: nowrap; text-align: left; }").appendTo("head");
+
+	$.fn.textStretch = function (options) {
+		var _dotextStretch, _useElementWidth, _recalc, _fontSize, _width, _i, $this, $elements = $(this);
+
+		// adding defaults
+		options = $.extend({
+			minFontSize : 0,
+			maxFontSize : Number.POSITIVE_INFINITY
+		}, options);
+
+		// no width specified. use element width (doesn't work for for inline or inline-blocks)
+		_useElementWidth = (options.width === undefined || options.width === null);
+
+		(_dotextStretch = function () {
+			for (_i = 0; _i < $elements.length; _i += 1) {
+				$this = $($elements[_i]);
+
+				// use element's width if no width specified
+				if (_useElementWidth) {
+					_width = $this.width();
+				} else {
+					if (typeof options.width !== "number") {
+						throw "$.textStretch error: Width is not a number";
+					}
+					_width = options.width;
+				}
+
+				// temporarily apply class for measuring width
+				$this.addClass("textStretch-calc");
+
+				// font size is calculated to: width of element / width of text / font-size
+				_fontSize = parseInt(_width / ($this.width() / (parseInt($this.css("fontSize"), 10) * 0.97)), 10);
+
+				// overwritten unless within specified font-size span
+				_fontSize = Math.min(Math.max(_fontSize, options.minFontSize), options.maxFontSize);
+
+				// remove measuring-class
+				$this.removeClass("textStretch-calc");
+
+				// apply font-size
+				$this.css("fontSize", _fontSize + "px");
+
+				// determine if a vertical scrollbar has been added or removed, in which case we have to recalculate
+				_recalc = (_useElementWidth && _width !== $this.width());
+			}
+		})();
+		// recalculate if needed
+		if (_recalc) {
+			_dotextStretch();
+		}
+		/* bind to resize ands viewport-change */
+		if (_useElementWidth) {
+			$(window).on("orientationchange resize", _dotextStretch);
+		}
+		return $elements;
+	};
+}(jQuery));
