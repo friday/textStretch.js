93493e5d0cb5f9b3b79a61ea23ab901501d7227e
Albin Larsson <mail@albinlarsson.com>
2013-12-09 21:42:13 +0100

    removing use of jQuery methods for performance and portability

--- jquery.textStretch.js
+++ jquery.textStretch.js
@@ -1,18 +1,17 @@
-﻿/**
- * textStretch.js pre-version alpha (2013.12.01)
+/**
+ * textStretch.js pre-version alpha (2013.12.09)
  *
  * Copyright (c) 2012, 2013 Albin Larsson (mail@albinlarsson.com)
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
- *
  **/
 
 (function ($) {
 	"use strict";
-	//  add class for calculating width. let me know if you see this and know a better way
-	$("<style>.textStretch-calc{ display: inline-block !important; *display: inline !important; white-space: nowrap !important; width: auto !important; text-align: left !important; }</style>").appendTo("head");
+	// add class for calculating width. let me know if you see this and know a better way (excluding insert/addRule)
+	$("<style>.textStretch-calc { display: inline-block !important; *display: inline !important; white-space: nowrap !important; width: auto !important; padding:0 !important; text-align: left !important; }</style>").appendTo("head");
 
 	$.fn.textStretch = function (options) {
-		var _settings, _dotextStretch, _useElementWidth, _recalc, _letterAverage, _fontSize, _width, _i, $this;
+		var _settings, _useElementWidth, _letterAverage, _fontSize, _width, _i, _recalc, element;
 
 		// import user settings/arguments
 		_settings = $.extend($.textStretch.defaults, options);
@@ -29,47 +28,49 @@
 		// no width specified. use element width (doesn't work for for inline or inline-blocks)
 		_useElementWidth = (_settings.width === 0);
 
-		_dotextStretch = function (elements) {
+		function _getStyle(elem, prop, camelProp) {
+			return elem.currentStyle ? elem.currentStyle[camelProp] : window.getComputedStyle(elem, null).getPropertyValue(prop);
+		}
+
+		function _textStretch(elements) {
 			for (_i = 0; _i < elements.length; _i += 1) {
-				$this = $(elements[_i]);
+				element = elements[_i];
 
 				// use element's width if no width specified
-				_width = _useElementWidth ? $this.width() : _settings.width;
-
-				_letterAverage = $this.data("textStretchLetterAverage");
+				_width = _useElementWidth ? element.clientWidth : _settings.width;
 
 				// checking if we already have pre-stored _letterAverage
-				if(_settings.refresh || !_letterAverage){
+				if(_settings.refresh || !(_letterAverage = element.getAttribute("data-textStretchLetterAverage"))){
 					// temporarily apply class for measuring width
-					$this.addClass("textStretch-calc");
+					element.className += " textStretch-calc";
 
-					// width of text / 90% of font-size (to be safe)
-					_letterAverage = ($this.width() / (parseInt($this.css("fontSize"), 10) * 0.90));
+					// width of text / 100%
+					_letterAverage = (element.clientWidth / (parseInt(_getStyle(element,"font-size", "fontSize"), 10) * 0.90));
 
 					// remove measuring-class
-					$this.removeClass("textStretch-calc");
+					element.className += element.className.substr(0, element.className.length - 17);
 
 					// store in element for faster regeneration
-					$this.data("textStretchLetterAverage", _letterAverage);
+					element.setAttribute("data-textStretchLetterAverage", _letterAverage);
 				}
 
 				// overwritten unless within specified font-size span
 				_fontSize = Math.min(Math.max(parseInt(_width / _letterAverage, 10), _settings.minFontSize), _settings.maxFontSize);
 
 				// apply font-size
-				$this.css("fontSize", _fontSize + "px");
+				element.style.fontSize = _fontSize + "px";
 
 				// determine if a vertical scrollbar has been added or removed, in which case we have to recalculate
-				_recalc = (_useElementWidth && _width !== $this.width());
+				_recalc = (_useElementWidth && _width !== element.clientWidth);
 			}
-		};
+		}
 
 		// run
-		_dotextStretch(this);
+		_textStretch(this);
 
 		// recalculate if needed
 		if (_recalc) {
-			_dotextStretch(this);
+			_textStretch(this);
 		}
 
 		// handle resize ands viewport-change. not needed for fixed width
@@ -80,7 +81,7 @@
 			// bind to events
 			if(!$.textStretch.eventIsBound){
 				$(window).on("orientationchange.textStretch resize.textStretch", function(){
-					_dotextStretch($.textStretch.elements);
+					_textStretch($.textStretch.elements);
 				});
 				$.textStretch.eventIsBound = true;
 			}
