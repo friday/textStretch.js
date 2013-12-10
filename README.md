648f15447f232d0282f5d8ea7cf28033a7461315
Albin Larsson <mail@albinlarsson.com>
2013-12-10 02:25:59 +0100

    removed 'width'-argument support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # textStretch.js
 
-A jQuery extension for stretching a text to the width of the element (or a fixed width).
+A jQuery extension for stretching a text to the width of the element.
 
 textStretch.js is minimal, fast, roubust, doesn't require or add any wrappers or children`, and it just works!
 
@@ -23,36 +23,32 @@ Load jQuery and the script.
 Apply textStretch to a set of elements
 
 ```javascript
-$(window).load(function () {
+$(function () {
   $(".stretch").textStretch();
 });
 ```
 
 It's that easy!
 
-### Options
-You can add some parameters to how Using the json-format
-
-* width - for a fixed width. If unspecified the elements width is used (which is probably what you want).
-* maxFontSize - Maximum font size (optional)
-* minFontSize - Minimum font size (optional)
+### Optional arguments
+* maxFontSize - Maximum font size
+* minFontSize - Minimum font size
 
 ```javascript
-$(".stretch").textStretch({width: 300}); // force width to 300
-$(".stretch").textStretch({minFontSize: 12}); // minimum font size
-$(".stretch").textStretch({minFontSize: 12, minFontSize: 64}); // min & max font-size
+$(".stretch").textStretch({ minFontSize: 12 }); // minimum font size
+$(".stretch").textStretch({ minFontSize: 12, minFontSize: 64 }); // min & max font-size
 ```
 
 You can also change the defaults
 
 ```javascript
-$.textStretch.defaults.minFontSize = 12; // minimum font size for future calls
-$(".stretch").textStretch(); // minFontSize is now 12
-$(".stretch").textStretch({minFontSize: 0}); // minFontSize disabled again
+$.textStretch.defaults.minFontSize = 12; // minimum font size for future calls, unless called with an overspecifying argument
+$(".stretch").textStretch(); // minFontSize is now 12, as defined above
+$(".stretch").textStretch({ minFontSize: 0 }); // minFontSize is disabled for this call
 ```
 
 ### Good to know
 * If using web fonts: Call from window.load (not document.ready) to be sure the font loads first.
 * Elements with `display:inline` or `display:inline-block` without a specified width will not stretch. It really wouldn't make sense if they did.
 * Will break in some cases if you use the `!important` css rule.
-* `line-height`, `letter-spacing` & `word-spacing won't scale if specified using `px` unit. See #8
+* `line-height`, `letter-spacing` and `word-spacing won't scale if specified using `px` unit. See [#8](https://github.com/friday/textStretch.js/issues/8)
--- jquery.textStretch.js
+++ jquery.textStretch.js
@@ -11,7 +11,7 @@
 	$("<style>.textStretch-calc { display: inline-block !important; *display: inline !important; white-space: nowrap !important; width: auto !important; padding:0 !important; text-align: left !important; font-size:100px !important }</style>").appendTo("head");
 
 	$.fn.textStretch = function (options) {
-		var _settings, _useElementWidth, _fontSize, _width, _i, _recalc, element;
+		var _settings, _fontSize, _boxWidth, _i, _recalc, element;
 
 		// import user settings/arguments
 		_settings = $.extend($.textStretch.defaults, options);
@@ -23,23 +23,20 @@
 					throw "textStretch error. Argument \"" + args[_i] + "\" must be a number. Argument given was \"" + _settings[args[_i]] + "\".";
 				}
 			}
-		}(["width", "minFontSize", "maxFontSize"]));
-
-		// no width specified. use element width (doesn't work for for inline or inline-blocks)
-		_useElementWidth = (_settings.width === 0);
+		}(["minFontSize", "maxFontSize"]));
 
 		function _textStretch(elements) {
 			for (_i = 0; _i < elements.length; _i += 1) {
 				element = elements[_i];
 
-				// use element's width if no width specified
-				_width = _useElementWidth ? element.clientWidth : _settings.width;
+				// get box width
+				_boxWidth = element.clientWidth;
 
 				// temporarily apply class for measuring width
 				element.className += " textStretch-calc";
 
 				// calculate new font size. 100 is the font-size
-				_fontSize = parseInt(_width / (element.clientWidth / 100), 10);
+				_fontSize = parseInt(_boxWidth / (element.clientWidth / 100), 10);
 
 				// overdefine if not within specified font-size span
 				_fontSize = Math.min(Math.max(_fontSize, _settings.minFontSize), _settings.maxFontSize);
@@ -48,14 +45,14 @@
 				element.style.fontSize = _fontSize + "px";
 
 				// sometimes new text will be too wide due to browsers rounding letter-spacing to even pixels
-				if(element.clientWidth > _width) {
+				if(element.clientWidth > _boxWidth) {
 					element.style.fontSize = _fontSize - 1 + "px";
 				}
 				// remove measuring-class
 				element.className = element.className.substr(0, element.className.length - 17);
 
 				// determine if a vertical scrollbar has been added or removed, in which case we have to recalculate
-				_recalc = (_useElementWidth && _width !== element.clientWidth);
+				_recalc = _boxWidth !== element.clientWidth;
 			}
 		}
 
@@ -67,24 +64,20 @@
 			_textStretch(this);
 		}
 
-		// handle resize ands viewport-change. not needed for fixed width
-		if (_useElementWidth) {
-			// add new elements to list.
-			$.textStretch.elements = $.textStretch.elements.add(this);
+		// add new elements to event-handler list.
+		$.textStretch.elements = $.textStretch.elements.add(this);
 
-			// bind to events
-			if(!$.textStretch.eventIsBound){
-				$(window).on("orientationchange.textStretch resize.textStretch", function(){
-					_textStretch($.textStretch.elements);
-				});
-				$.textStretch.eventIsBound = true;
-			}
+		// bind to events
+		if(!$.textStretch.eventIsBound){
+			$(window).on("orientationchange.textStretch resize.textStretch", function(){
+				_textStretch($.textStretch.elements);
+			});
+			$.textStretch.eventIsBound = true;
 		}
 		return this;
 	};
 	$.textStretch = {
 		defaults: {
-			width: 0,
 			minFontSize: 0,
 			maxFontSize: Number.POSITIVE_INFINITY
 		},
